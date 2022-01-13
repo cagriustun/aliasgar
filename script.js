c1f8c1cd74a0a08d13129385e3ecdd06f48610d9
Çağrı Üstün <cagri-ustun@hotmail.com>
2022-01-13 11:27:37 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,16 @@
-var panorama, viewer, container, infospot;
 
-container = document.querySelector('#container');
 
-panorama = new PANOLENS.ImagePanorama('a.jpg');
+// create PanoViewer with option
+var PanoViewer = eg.view360.PanoViewer;
+var container = document.getElementById("myPanoViewer");
+var panoViewer = new PanoViewer(container, {
+  image: "a.jpg",
+  projectionType: "equirectangular" });
 
-infospot = new PANOLENS.Infospot(350, PANOLENS.DataImage.Info);
-infospot.position.set(0, -2000, -5000);
 
-panorama.add(infospot);
-
-viewer = new PANOLENS.Viewer({ container: container });
-// setInterval(() => {
-//     console.log(infospot);
-// }, 1000);
-// container.addEventListener("click", function() { DeviceMotionEvent.requestPermission(); });
-// var aset = setInterval(() => {
-//     if(viewer.DeviceOrientationControls.enabled){
-//         console.log("mobile");
-//         DeviceMotionEvent.requestPermission();
-//         clearInterval(aset);
-//     }
-// }, 500);
-if (typeof DeviceOrientationEvent.requestPermission === 'function') {
-    document.body.addEventListener('click', function () {
-    DeviceOrientationEvent.requestPermission()
-    .then(function() {
-    console.log('DeviceOrientationEvent, DeviceMotionEvent enabled');
-    alert('DeviceOrientationEvent, DeviceMotionEvent enabled');
-    })
-    .catch(function (error) {
-    console.warn('DeviceOrientationEvent, DeviceMotionEvent not enabled', error);
-    alert('DeviceOrientationEvent, DeviceMotionEvent not enabled', error);
-    })
-    });
-    }
-viewer.add(panorama);
-viewer.addUpdateCallback(function () {
-
+var panoviewerSet = document.getElementById("panoSet");
+PanoControls.init(panoviewerSet, panoViewer, {
+  //enableGyroOption: true,
+  //enableTouchOption: true
 });
+PanoControls.showLoading();
