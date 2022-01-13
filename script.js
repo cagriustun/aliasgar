6c4e1d5dd0ab8ad85bfa5a6f05efc4d570f39d35
Çağrı Üstün <cagri-ustun@hotmail.com>
2022-01-13 10:36:38 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 var panorama, viewer, container, infospot;
 
-container = document.querySelector( '#container' );
+container = document.querySelector('#container');
 
-panorama = new PANOLENS.ImagePanorama( 'a.jpg' );
+panorama = new PANOLENS.ImagePanorama('a.jpg');
 
-infospot = new PANOLENS.Infospot( 350, PANOLENS.DataImage.Info );
-infospot.position.set( 0, -2000, -5000 );
+infospot = new PANOLENS.Infospot(350, PANOLENS.DataImage.Info);
+infospot.position.set(0, -2000, -5000);
 
-panorama.add( infospot );
+panorama.add(infospot);
 
-viewer = new PANOLENS.Viewer( { container: container } );
-viewer.add( panorama );
+viewer = new PANOLENS.Viewer({ container: container });
+// setInterval(() => {
+//     console.log(infospot);
+// }, 1000);
+container.addEventListener("click", function() { DeviceMotionEvent.requestPermission(); });
+viewer.add(panorama);
+viewer.addUpdateCallback(function () {
 
-viewer.addUpdateCallback(function(){
-  
 });
